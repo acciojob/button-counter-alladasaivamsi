6892ba16b989ee550bdba2c2925319a337a92b5e
alladasaivamsi <alladasaivamsi@gmail.com>
2023-06-16 14:51:51 +0530

    first change

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const [count , setCount] = useState(0);
   return (
     <div>
-        <h1>"Button clicked " + {count} + " times"</h1>
+        <h1>Button clicked  {count} times </h1>
         <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   )
